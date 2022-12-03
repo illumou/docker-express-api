d5d0f3a9fa26c941e09a86f9ea389aa1c12f89ab
Robin <82582947+illumou@users.noreply.github.com>
2022-12-03 18:37:17 +0100

    add .env

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import * as dotenv from 'dotenv'
 import { router }  from './routes'
 const app = express();
+
+dotenv.config()
 
 app.use(express.json())
 app.use('/api', router)
 
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log(`App listen on PORT ${port}`)
