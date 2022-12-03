0aa9c51d5c68d88a0c64764380003948f4c9c851
Robin <82582947+illumou@users.noreply.github.com>
2022-12-03 18:59:15 +0100

    remove dist

--- dist/routes/bikes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BikeRouter = void 0;
-const BikeRouter = require('express').Router();
-exports.BikeRouter = BikeRouter;
-BikeRouter.get('/', (req, res) => {
-    res.send(true);
-});
